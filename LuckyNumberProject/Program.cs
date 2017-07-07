53f8d1bfce69c2cbc5c49252fd8de784e84c7f29
Frank Nasseri <franknasseri57@gmail.com>
2017-07-07 14:52:51 -0400

    adding to second project

--- LuckyNumberProject/Program.cs
+++ LuckyNumberProject/Program.cs
@@ -10,6 +10,40 @@ namespace LuckyNumberProject
     {
         static void Main(string[] args)
         {
+            //PART ONE
+            // Ask the user for a starting number for the lowest number in the number range.
+            Console.WriteLine("Please insert a starting number low number, higher than zero");
+            int firstNumber = int.Parse(Console.ReadLine());
+            //Ask the user for an ending number for the highest number in the number range.
+            Console.WriteLine("Please insert an ending number for the highest number in the number range");
+            int secondNumber = int.Parse(Console.ReadLine());
+            //Ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
+            // Numbers must be stored in an array
+            // Array must be populated using a loop
+            //If the user enters a number that is outside of the range set, prompt the user to give you a valid number. 
+            //Do this until the user enters a valid number.
+            int[] userGuess = new int[6];
+            for (int i = 0; i <= userGuess.Length - 1; i++)
+            {
+                Console.WriteLine("Guess a number:");
+                userGuess[i] = int.Parse(Console.ReadLine());
+
+                while (userGuess[i] > firstNumber && userGuess[i] < secondNumber)
+                {
+                    userGuess[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Please insert a number between" + " " + firstNumber + " - " + secondNumber);
+            }
+            //PART TWO
+            //the program should randomly generate 6 numbers using a loop
+
+
+
+
+            
+
+
         }
     }
 }
